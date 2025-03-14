4a0c13c1cc63eb5319a3692323b0acb5e381b8f9
techwithtwin <realwebtechguru@gmail.com>
2025-03-14 11:41:03 +0300

    my starter

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,5 +1,9 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function TabTwoScreen() {
-  return <View style={{ backgroundColor: "red", flex: 1 }}></View>;
+  return (
+    <View style={{ backgroundColor: "red", flex: 1 }}>
+      <Text>Explore</Text>
+    </View>
+  );
 }
